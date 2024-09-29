3f1d7c5cb6fa00aed91f9257b6a58f7208f7acea
Yunwei Dai <daiyunwei1998@gmail.com>
2024-09-30 02:27:10 +0800

    Refactor main.py to include aggregation_router in app.routers

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from app.core.config import settings
 from app.dependencies import get_db
 from app.exceptions.tenant_exceptions import DuplicateTenantNameException, DuplicateTenantAliasException
 from app.models.tenant import Tenant, Base
-from app.routers import usage_router
+from app.routers import usage_router, aggregation_router
 from app.schemas.tenant_schema import TenantCreateSchema, TenantInfoSchema, TenantUpdateSchema
 from app.services.image_upload import upload_to_s3
 from app.services.task_complete_message_handler import start_message_handler
@@ -30,7 +30,7 @@ app.include_router(upload_router, prefix="/files")
 app.include_router(knowlege_base_router)
 app.include_router(tenant_doc_router)
 app.include_router(usage_router.router)
-app.include_router(aggregation_router.router) 
+app.include_router(aggregation_router.router)
 
 # CORS Middleware
 app.add_middleware(
